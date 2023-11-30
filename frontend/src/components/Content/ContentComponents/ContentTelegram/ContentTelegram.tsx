98cde8d51a518d3b6a3c69bfa090bf06c8cb2f90
Alexander <sashafootball2018@gmail.com>
2023-11-30 22:47:41 +0300

    Mobile is ready

--- frontend/src/components/Content/ContentComponents/ContentTelegram/ContentTelegram.tsx
+++ frontend/src/components/Content/ContentComponents/ContentTelegram/ContentTelegram.tsx
@@ -3,9 +3,14 @@ import styles from './ContentTelegram.module.css'
 import tgLogo  from '../../../../assets/images/tgLogo.svg'
 
 const ContentTelegram: FC = () => {
+	const handleTelegramButton = (): void => {
+		window.open('https://t.me/Lilmint11', '_blank')
+	}
+
+
 	return (
 		<div className={styles.contentTelegramContainer}>
-			<button className={styles.contentTelegram}>
+			<button onClick={handleTelegramButton} className={styles.contentTelegram}>
 				<div className={styles.contentTelegramFilling}>
 					<div className={styles.contentTelegramIcon}>
 						<img alt='tgIcon' src={tgLogo} />
